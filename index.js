6dd950ce7fb97010f2426aa688189eccd767b801
jako2889 <jako2889@stud.kea.dk>
2018-05-01 10:11:59 +0200

    js start

--- index.js
+++ index.js
@@ -9,4 +9,31 @@ function sidenvises() {
     $("#celeb_container").addClass("celeb_positionstart");
     $("#kvinde_container").addClass("kvinde_positionstart");
     $("#phone_container").addClass("phone_positionin");
+    $("#phone_container").hide();
+    $("#doer_sprite").addClass("doer_open");
+
+    setTimeout(doer_close, 3000);
+}
+
+function doer_close() {
+    console.log("dreng kommer ind");
+
+    $("#dreng_container").addClass("dreng_movein");
+    $("#dreng_sprite").addClass("dreng_walkcycle");
+
+    $("#dreng_container").on("animationend", scene_skift);
+}
+
+function scene_skift() {
+    console.log("skifter til pov vinkel");
+
+    $("#dreng_container").off("animationend", scene_skift);
+
+    $("#dreng_container").removeClass("dreng_movein");
+    $("#dreng_sprite").removeClass("dreng_walkcycle");
+    $("#doer_sprite").removeClass("doer_open");
+
+    $("#phone_container").show();
+    $("#doer_sprite").addClass("doer_closed");
+    $("#dreng_container").addClass("dreng_positionin");
 }
