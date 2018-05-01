8fc0bc0987ce94f8395d57dcd0e4471527ea02f6
jako2889 <jako2889@stud.kea.dk>
2018-05-01 09:48:23 +0200

    start js added

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ $(window).on("load", sidenvises);
 
 function sidenvises() {
     console.log("Start af spillet");
+
+    $("#dreng_container").addClass("dreng_positionstart");
+    $("#noerd_container").addClass("noerd_positionstart");
+    $("#kat_container").addClass("kat_positionstart");
+    $("#celeb_container").addClass("celeb_positionstart");
+    $("#kvinde_container").addClass("kvinde_positionstart");
 }
